1b827c02448a40ff60cc73da11b34a5efea74dff
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-23 23:35:22 +0000

    2.3 complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,26 +2,23 @@ public class PracticeProblem {
 
 	public static void main(String args[]) {
 
-	}
+		
 
-	public static void q1() {
-		//Write question 1 code here
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
+	public static void bubbleSortString(String[] strings){
+		boolean swapped=true;
+		for(int i=0; i<strings.length-1 && swapped;i++){
+			swapped=false;
+			for (int j=0;j<strings.length-i-1; j++){
+				if(strings[j].toLowerCase().compareTo(strings[j+1].toLowerCase())>0){
+					swapped=true;
+					String temp=strings[j];
+					strings[j]=strings[j+1];
+					strings[j+1]=temp;
+					
+				}
+			}
+		}
 	}
 
 }
